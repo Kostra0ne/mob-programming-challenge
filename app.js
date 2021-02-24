766364fec0bcb543e1d281dafdccc9896cb4e069
august159 <augustin.leman@gmail.com>
2021-02-24 11:42:44 +0100

    Iteration 9 completed

--- app.js
+++ app.js
@@ -11,23 +11,25 @@ app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
-hbs.registerPartials(__dirname + "/views/partials");npm
+hbs.registerPartials(__dirname + "/views/partials");
+npm;
 
-const users = [{
-    name:"Mathieu", 
+const users = [
+  {
+    name: "Mathieu",
     email: "mathieu@coucou.lesami.es",
-    favoriteLanguage: "Not English apparently"
-},
-{
-    name:"Toto", 
+    favoriteLanguage: "Not English apparently",
+  },
+  {
+    name: "Toto",
     email: "toto@coucou.lesami.es",
-    favoriteLanguage: "Not JS apparently"
-},
-{
-    name:"Myriam", 
+    favoriteLanguage: "Not JS apparently",
+  },
+  {
+    name: "Myriam",
     email: "myriam@coucou.lesami.es",
-    favoriteLanguage: "Not MongoDB apparently"
-}
+    favoriteLanguage: "Not MongoDB apparently",
+  },
 ];
 
 const images = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg"];
@@ -35,26 +37,29 @@ const images = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg"];
 console.log("Hello best team ♥");
 
 //ROUTES
-app.get("/", (req,res)=>{
-    res.render("home");
-    {images} 
+app.get("/", (req, res) => {
+  res.render("home");
+  {
+    images;
+  }
 });
-app.get("/my-dev-squad", (req, res)=>{
-    res.render("allUsers");
-    {users}
+app.get("/my-dev-squad", (req, res) => {
+  res.render("allUsers");
+  {
+    users;
+  }
 });
-app.get("/add-new-ironhacker", (req, res)=>{
-    res.render("formUser");
+app.get("/add-new-ironhacker", (req, res) => {
+  res.render("formUser");
 });
-app.get("*", (req, res)=>{
-    res.send("Sorry not found");
+app.get("*", (req, res) => {
+  res.send("Sorry not found");
 });
 
 //app.get("/api/ironhackers", (req, res)=>{
 //  res.json("users")
 //});
 
-
 app.listen(process.env.PORT, () => {
-    console.log("Ready @ http://localhost:" + process.env.PORT);
+  console.log("Ready @ http://localhost:" + process.env.PORT);
 });
